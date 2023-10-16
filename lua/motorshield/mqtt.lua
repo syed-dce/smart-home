c4cb0639f8dd39cd00d828f715c964672496d1eb
syed-dce <syed7771@yahoo.com>
2023-10-17 00:14:16 +0530

    mqtt added

--- lua/motorshield/mqtt.lua
+++ lua/motorshield/mqtt.lua
@@ -1,8 +1,11 @@
+local servo = require "servo"
+local motor = require "motor"
+
 -- Holds dispatching keys to different topics. Serves as a makeshift callback
 -- function dispatcher based on topic and message content
 m_dis = {}
 
-MQTT_CLIENTID = "motorshield"
+MQTT_CLIENTID = "motor"
 MQTT_HOST = "192.168.1.206"
 MQTT_PORT = 1883
 
@@ -10,54 +13,96 @@ MQTT_PORT = 1883
 local count = 0
 local time = 0
 
-gpio.mode(4, gpio.OUTPUT)
-gpio.write(4, gpio.HIGH)
 
-tmr.stop(2)
-
-pwm.setup(4,1000,1000);
-pwm.start(4);
-
-function animate(m, pl)
+function run_forward(m, pl)
     -- Confirm that an animation message was received on the /mcu/cmd topic
-    m:publish("/mcu/led/", "--> ANIMATE COMMAND", 0, 0,
-            function(m) print("ANIMATE COMMAND") end)
+    m:publish("/motor/stat/run", "forward " .. pl, 0, 0,
+            function(m) print("run: forward" .. pl) end)
     
-    -- Main option control structure. Pretty gross-looking but it works
-    -- Option 0 turns everything off
+    motor.set("A", "F", pl)
+    motor.set("B", "F", pl)
+end
+
+
+function run_backward(m, pl)
+    -- Confirm that an animation message was received on the /mcu/cmd topic
+    m:publish("/motor/stat/run", "backward " .. pl, 0, 0,
+            function(m) print("run: backward" .. pl) end)
+    
+    motor.set("A", "R", pl)
+    motor.set("B", "R", pl)
+end
+
+
+function turn_left(m, pl)
+    -- Confirm that an animation message was received on the /mcu/cmd topic
+    m:publish("/motor/stat/run", "left " .. pl, 0, 0,
+            function(m) print("run: left" .. pl) end)
+    
+    motor.set("A", "F", pl)
+    motor.set("B", "R", pl)
+end
+
+
+function turn_right(m, pl)
+    -- Confirm that an animation message was received on the /mcu/cmd topic
+    m:publish("/motor/stat/run", "right " .. pl, 0, 0,
+            function(m) print("run: right" .. pl) end)
+    
+    motor.set("A", "B", pl)
+    motor.set("B", "F", pl)
+end
+
+
+function stop(m, pl)
+    -- Confirm that an animation message was received on the /mcu/cmd topic
+    m:publish("/motor/stat/run", "stop", 0, 0,
+            function(m) print("stop") end)
+    
+    motor.set("A", "R", 0)
+    motor.set("B", "R", 0)
+end
+
+
+function light(m, pl)
     if pl == "0" then
         -- Confirm LED being turned off to serial terminal and MQTT broker
-        m:publish("/mcu/led/", "OFF", 0, 0,
-            function(m) print("LED OFF") end)
-
-        gpio.write(4, gpio.HIGH)
-      
+        m:publish("/motor/stat/light", "OFF", 0, 0,
+            function(m) print("Light OFF") end)
+        --gpio.write(light_pin, gpio.HIGH)    
     end
     if pl == "1" then
         -- Confirm LED being turned off to serial terminal and MQTT broker
-        m:publish("/mcu/led/", "ON", 0, 0,
-            function(m) print("LED ON") end)
-
-        gpio.write(4, gpio.LOW)
-      
+        m:publish("/motor/stat/light", "ON", 0, 0,
+            function(m) print("Light ON") end)
+        --gpio.write(light_pin, gpio.LOW)
     end
 end
 
-function dim(m, pl)
+
+function move_servo(m, pl)
     -- Confirm that an animation message was received on the /mcu/cmd topic
-    m:publish("/mcu/led/", "--> DIM COMMAND", 0, 0,
-            function(m) print("DIM COMMAND") end)
-   
-    if pl ~= nil then
-        pwm.setduty(4,1000-pl*10);
+    m:publish("/motor/stat/servo", "servo: " .. pl, 0, 0,
+            function(m) print("servo: " .. pl) end)
+
+    local args = {}
+    for arg in string.gmatch(pl, "([^,]+)") do
+        table.insert(args, arg)
     end
+    servo.set(tonumber(args[1]),tonumber(args[2]))
 end
 
     
 -- As part of the dispatcher algorithm, this assigns a topic name as a key or
 -- index to a particular function name
-m_dis["/mcu/cmd/led"] = animate
-m_dis["/mcu/cmd/dim"] = dim
+m_dis["/motor/cmd/forward"] = run_forward
+m_dis["/motor/cmd/backward"] = run_backward
+m_dis["/motor/cmd/left"] = turn_left
+m_dis["/motor/cmd/right"] = turn_right
+m_dis["/motor/cmd/stop"] = stop
+m_dis["/motor/cmd/light"] = light
+m_dis["/motor/cmd/servo"] = move_servo
+
 
 -- initialize mqtt client with keepalive timer of 60sec
 m = mqtt.Client(MQTT_CLIENTID, 60, "", "") -- Living dangerously. No password!
@@ -66,7 +111,7 @@ m = mqtt.Client(MQTT_CLIENTID, 60, "", "") -- Living dangerously. No password!
 -- Set up Last Will and Testament (optional)
 -- Broker will publish a message with qos = 0, retain = 0, data = "offline"
 -- to topic "/lwt" if client don't send keepalive packet
---m:lwt("/lwt", "Oh noes! Plz! I don't wanna die!", 0, 0)
+m:lwt("/lwt", "Oh noes! Plz! I don't wanna die!", 0, 0)
 
 
 -- When client connects, print status message and subscribe to cmd topic
@@ -76,41 +121,17 @@ m:on("connect", function(m)
         " on port ", MQTT_PORT, "\n\n")
 
     -- Subscribe to the topic where the ESP8266 will get commands from
-    m:subscribe("/mcu/cmd/#", 0,
+    m:subscribe("/motor/cmd/#", 0,
         function(m) print("Subscribed to CMD Topic") end)
-
-    tmr.alarm(2, 1000, 1, function ()
-            time = time + 1
-            m:publish("/mcu/stat/uptime", time, 0, 0,
-            function(m) print("Sent:" .. time) end)
-    end)
-
-    tmr.alarm(5, 5000, 1, function ()
-            if (temp ~= nil ) then
-                local str = string.format("%0.1f", temp)
-                m:publish("/mcu/stat/temp", str, 0, 0,
-                function(m) print("Temp:" .. str) end)
-            end
-            if (dhttemp ~= nil ) then
-                local str = string.format("%0.1f", dhttemp)
-                m:publish("/mcu/stat/dhttemp", str, 0, 0,
-                function(m) print("DHTTemp:" .. str) end)
-            end
-            if (dhthumi ~= nil ) then
-                local str = string.format("%d", dhthumi)
-                m:publish("/mcu/stat/humi", str, 0, 0,
-                function(m) print("DHThumi:" .. str) end)
-            end
-    end)
         
 end)
 
 
 -- When client disconnects, print a message and list space left on stack
---m:on("offline", function(m)
---    print ("\n\nDisconnected from broker")
---    print("Heap: ", node.heap())
---end)
+m:on("offline", function(m)
+    print ("\n\nDisconnected from broker")
+    print("Heap: ", node.heap())
+end)
 
 
 -- On a publish message receive event, run the message dispatcher and

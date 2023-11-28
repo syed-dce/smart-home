e31e121a581b4c7801d2b96a471271b3f5394901
syed-dce <syed7771@yahoo.com>
2023-11-29 00:22:36 +0530

    Telnet module replaced

--- lua/common/telnet.lua
+++ lua/common/telnet.lua
@@ -1,24 +1,42 @@
-    -- a simple telnet server
-    s=net.createServer(net.TCP,180)  
-    s:listen(2323,function(c)
-       function s_output(str)
-          if(c~=nil) then
-             c:send(str .. "\n")
-          end
-       end
-       node.output(s_output, 0)   -- re-direct output to function s_ouput.
-       c:on("receive",function(c,l)
-            if (l:byte(1) == 13) then
-                if (abort == false) then
-                abort = true
-                print("Abort") 
-                end 
-            end    
-            node.input(l)           -- works like pcall(loadstring(l)) but support multiple separate line
-       end)
-       c:on("disconnection",function(c)
-          node.output(nil)        -- un-regist the redirect output function, output goes to serial
-       end)
-       print("Welcome to NodeMcu world.")
+-- a simple telnet server
+
+-- restart server if needed
+if telnet_srv ~= nil then
+    telnet_srv:close()
+end
+telnet_srv = net.createServer(net.TCP, 180)
+
+telnet_srv:listen(23, function(socket)
+    local fifo = {}
+    local fifo_drained = true
+
+    local function sender(c)
+        if #fifo > 0 then
+            c:send(table.remove(fifo, 1))
+        else
+            fifo_drained = true
+        end
+    end
+
+    local function s_output(str)
+        table.insert(fifo, str)
+        if socket ~= nil and fifo_drained then
+            fifo_drained = false
+            sender(socket)
+        end
+    end
+
+    node.output(s_output, 0)   -- re-direct output to function s_ouput.
+
+    socket:on("receive", function(c, l)
+        node.input(l)           -- works like pcall(loadstring(l)) but support multiple separate line
     end)
-    print("Telnet server started.")
+    socket:on("disconnection", function(c)
+        node.output(nil)        -- un-regist the redirect output function, output goes to serial
+    end)
+    socket:on("sent", sender)
+
+    print("Welcome to NodeMCU world.")
+end)
+
+print("Telnet server running...")

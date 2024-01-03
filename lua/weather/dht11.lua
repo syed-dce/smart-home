17ba07848b2e8e22fe001e0b324d893f2171b522
syed-dce <syed7771@yahoo.com>
2024-01-04 02:03:52 +0530

    Added DHT sensor error status

--- lua/weather/dht11.lua
+++ lua/weather/dht11.lua
@@ -6,12 +6,16 @@ tmr.alarm(DHT_ALARM_ID, REQUEST_PERIOD, 1, function ()
     status, temp, humi, temp_dec, humi_dec = dht.read(GPIO_DHT)
 
     if status == dht.OK then
-        print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+        print("DHT: "..temp.."'C "..humi.."%")
         dhttemp = temp
         dhthumi = humi
     elseif status == dht.ERROR_CHECKSUM then
+        dhttemp = "break"
+        dhthumi = "break"
         print( "DHT Checksum error." )
     elseif status == dht.ERROR_TIMEOUT then
+        dhttemp = "break"
+        dhthumi = "break"
         print( "DHT timed out." )
     end
 end)

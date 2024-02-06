e1f5fb82ee642c541c9b0e75716ac94fa32b4477
syed-dce <syed7771@yahoo.com>
2024-02-07 02:08:35 +0530

    Socket topic updated

--- python/bluediscovery.py
+++ python/bluediscovery.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 
-import sys, bluetooth, time
+import sys, bluetooth, time, os
 import paho.mqtt.client as mqtt
 
 MQTT_SERVER = "192.168.1.206"
 MQTT_PORT = 1883
 MQTT_CLIENT_ID = "bluediscover"
 
-targets = {"78:02:F8:03:68:30":"/presence/mibt"}
+targets = {"78:02:F8:03:68:30":"/presence/mibt",
+           "4C:49:E3:24:6A:8A":"/presence/mi5cbt"}
+
 
 client = mqtt.Client(MQTT_CLIENT_ID)
-
+connected = False
 
 def on_connect(client, userdata, rc):
     client.subscribe("/f", 0)
-    print "Connected"
+    print "Connected."
+    global connected
+    connected = True
 
 def on_message(mosq, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def connect_broker():
-    print "Connecting to broker %s..." %(MQTT_SERVER)
+    print "Connecting to broker %s..." %(MQTT_SERVER),
     try:
         rc = client.connect(MQTT_SERVER, MQTT_PORT, 60)
     except Exception as detail:
@@ -36,18 +40,18 @@ def connect_broker():
 
 
 def scan():
-    nearby_devices = bluetooth.discover_devices(lookup_names=True)
-    print "Found %d devices" % len(nearby_devices)
+    devices = bluetooth.discover_devices(lookup_names=True)
+    print "Found %d devices" % len(devices)
     
-    for addr, name in nearby_devices:
+    for addr, name in devices:
         print "  %s - %s" % (addr, name)
         
-    return nearby_devices
+    return devices
 
 
 def monitor():
     while True:
-        print "Scanning.."
+        print "\r\nScanning.."
         nearby_devices = scan()
         
         for addr in targets:
@@ -58,25 +62,20 @@ def monitor():
                     break
                 else:
                     continue
-            print "%s - %s" %(targets[addr], msg)
+            print "Publish: %s - %s" %(targets[addr], msg)
             client.publish(targets[addr], msg)
-                  
-            
-#         for addr, name in nearby_devices:
-#             if targets.get(addr):
-#                 msg = "1"
-#             else:
-#                 msg = "0"
-#                 
-#             print "%s - %s" %(targets[addr], msg)
-#             client.publish(targets[addr], msg)
-                
+
         time.sleep(10)
                 
 
 def main():
-    connect_broker()
-    monitor()
+    try:
+        connect_broker()
+        while not connected:
+            time.sleep(0.5)
+        monitor()
+    except (KeyboardInterrupt, SystemExit):
+        print "\r\nExit"
 
 
 if  __name__ ==  "__main__" : main()

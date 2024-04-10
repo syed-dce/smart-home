3f1ddabcb0b9f97b60db015a6a241511a811ba56
syed-dce <syed7771@yahoo.com>
2024-04-11 02:13:37 +0530

    Added state reporting to switch_power function

--- lua/socket/broker.lua
+++ lua/socket/broker.lua
@@ -23,13 +23,23 @@ end
 
 -- actions
 local function switch_power(m, pl)
+    local msg
 	if pl == "ON" or pl == "1" then
 		gpio.write(GPIO_SWITCH, gpio.LOW)
-		print("MQTT : plug ON for ", MQTT_CLIENTID)
+        print("MQTT : plug ON for ", MQTT_CLIENTID)
+        msg = "ON"
 	elseif pl == "OFF" or pl == "0" then
 		gpio.write(GPIO_SWITCH, gpio.HIGH)
-		print("MQTT : plug OFF for ", MQTT_CLIENTID)
-	end
+        print("MQTT : plug OFF for ", MQTT_CLIENTID)
+        msg = "OFF"
+    end
+    if connected then
+        m:publish(MQTT_MAINTOPIC .. '/state/power', msg, 0, 1)
+        print("MQTT (online): " .. msg)
+        LedBlink(100)
+    else
+        print("MQTT (offline): " .. msg)
+    end
 end
 
 local function toggle_power()
